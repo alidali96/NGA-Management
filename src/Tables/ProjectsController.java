819db6c3c643e885e2edde73dcf7a54b0826a99d
Ghaith Darwish <ghaith.darwish01@stclairconnect.ca>
2019-10-28 17:46:30 -0400

    Create the layout for each table

--- src/Tables/ProjectsController.java
+++ src/Tables/ProjectsController.java
@@ -16,51 +16,54 @@ import java.net.URL;
 import java.util.Observable;
 import java.util.ResourceBundle;
 
-public class ProjectsController implements Initializable {
+public class ProjectsController{
+
+   // public class ProjectsController implements Initializable {
 
 
-    @FXML
-    private JFXTreeTableView table;
-
-    @FXML
-    private JFXTreeTableColumn<Example, String> id;
-
-    @FXML
-    private JFXTreeTableColumn<Example, String> id1;
-
-    @FXML
-    private JFXTreeTableColumn<Example, String> id2;
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-        id.setCellValueFactory(param -> param.getValue().getValue().fmane);
-        id1.setCellValueFactory(param -> param.getValue().getValue().lName);
-        id2.setCellValueFactory(param -> param.getValue().getValue().age);
-
-        table.getColumns().setAll(id, id1, id2);
-
-        ObservableList<Example> example = FXCollections.observableArrayList();
-        example.add(new Example("Ghaith", "Darwish", "25"));
-        example.add(new Example("Ghaith", "Darwish", "25"));
-        example.add(new Example("Ghaith", "Darwish", "25"));
-        example.add(new Example("Ghaith", "Darwish", "25"));
-
-
-        TreeItem<Example> data = new RecursiveTreeItem<Example>(example, RecursiveTreeObject::getChildren);
-        table.setRoot(data);
-        table.setShowRoot(false);
-    }
-
-
-    class Example extends RecursiveTreeObject<Example>{
-        StringProperty fmane;
-        StringProperty lName;
-        StringProperty age;
-        public Example (String fname, String lName, String age){
-            this.fmane = new SimpleStringProperty(fname);
-            this.lName = new SimpleStringProperty(lName);
-            this.age = new SimpleStringProperty(age);
-        }
-    }
+//    @FXML
+//    private JFXTreeTableView table;
+//
+//    @FXML
+//    private JFXTreeTableColumn<Example, String> id;
+//
+//    @FXML
+//    private JFXTreeTableColumn<Example, String> id1;
+//
+//    @FXML
+//    private JFXTreeTableColumn<Example, String> id2;
+//
+//    @Override
+//    public void initialize(URL location, ResourceBundle resources) {
+//
+//        id.setCellValueFactory(param -> param.getValue().getValue().fmane);
+//        id1.setCellValueFactory(param -> param.getValue().getValue().lName);
+//        id2.setCellValueFactory(param -> param.getValue().getValue().age);
+//
+//        table.getColumns().setAll(id, id1, id2);
+//
+//
+//        ObservableList<Example> example = FXCollections.observableArrayList();
+//        example.add(new Example("Ghaith", "Darwish", "25"));
+//        example.add(new Example("Ghaith", "Darwish", "25"));
+//        example.add(new Example("Ghaith", "Darwish", "25"));
+//        example.add(new Example("Ghaith", "Darwish", "25"));
+//
+//
+//        TreeItem<Example> data = new RecursiveTreeItem<Example>(example, RecursiveTreeObject::getChildren);
+//        table.setRoot(data);
+//        table.setShowRoot(false);
+//    }
+//
+//
+//    class Example extends RecursiveTreeObject<Example>{
+//        StringProperty fmane;
+//        StringProperty lName;
+//        StringProperty age;
+//        public Example (String fname, String lName, String age){
+//            this.fmane = new SimpleStringProperty(fname);
+//            this.lName = new SimpleStringProperty(lName);
+//            this.age = new SimpleStringProperty(age);
+//        }
+//    }
 }
