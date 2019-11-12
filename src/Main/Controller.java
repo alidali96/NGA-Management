46a09cbee0d98dddbfdf085d373e9055a4d83ceb
Ndricim Strazimiri <ns10@myscc.ca>
2019-11-11 22:29:44 -0500

    Install fontawesome to the project #25 * Added icons to the buttons * Added .jar to the project to load FontAwesome Icons

--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -9,11 +9,16 @@ import Database.CSP.Status.StatusDAO;
 import Database.DatabaseConnection;
 import Database.CSP.Status.TestStatus;
 import Database.Project.TestProject;
+import com.jfoenix.controls.JFXButton;
+import de.jensd.fx.glyphs.GlyphIcons;
+import de.jensd.fx.glyphs.GlyphsDude;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
@@ -21,9 +26,15 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static de.jensd.fx.glyphs.GlyphsDude.createIcon;
+import static de.jensd.fx.glyphs.GlyphsDude.createIconLabel;
+
 public class Controller implements Initializable {
     @FXML
     AnchorPane tableContainer;
+
+    @FXML
+    JFXButton projectsButton;
 
 
 
@@ -45,7 +56,8 @@ public class Controller implements Initializable {
 
         // Test Project Table Class
 //        TestProject testProject = new TestProject();
-
+//        Label icon=createIconLabel("Project");
+//        projectsButton.setGraphic(GlyphsDude.createIcon(FontAwesomeIcons.BRIEFCASE,"13px"));
 
     }
 
