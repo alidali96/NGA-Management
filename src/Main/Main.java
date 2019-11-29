e592306b0cbeea9b007be07d380d661102459500
Ali D <55010336+alidali96@users.noreply.github.com>
2019-11-28 23:11:24 -0500

    Revert "Database"

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,34 +1,20 @@
 package Main;
 
-import Database.DatabaseConnection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
-
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-//        DatabaseConnection.getInstance();
-        //run sucker
-        Parent root = FXMLLoader.load(getClass().getResource("/views/DBLoginView.fxml"));
-
-        Scene scene = new Scene(root);
-        scene.setFill(Color.TRANSPARENT);
-        primaryStage.setScene(scene);
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("NGA Management");
+        primaryStage.setScene(new Scene(root));
         primaryStage.centerOnScreen();
-//        primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.initStyle(StageStyle.TRANSPARENT);
-        primaryStage.setResizable(false);
         primaryStage.show();
-
     }
 
 
