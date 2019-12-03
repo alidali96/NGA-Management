0c20caf53148c6003b174a1a5117f4824ea08413
Ghaith Darwish <ghaith.darwish01@stclairconnect.ca>
2019-12-03 16:31:47 -0500

    Added Categories Statistic

--- src/controllers/StatisticController.java
+++ src/controllers/StatisticController.java
@@ -1,11 +1,11 @@
 package controllers;
 
-import Const.Const;
 import Database.CSP.Category.Category;
 import Database.CSP.Category.CategoryDAO;
+import Database.CSP.Priority.Priority;
+import Database.CSP.Priority.PriorityDAO;
 import Database.CSP.Status.Status;
 import Database.CSP.Status.StatusDAO;
-import Database.Project.Project;
 import Database.Project.ProjectDAO;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -24,9 +24,10 @@ public class StatisticController implements Initializable {
     ProjectDAO projectDAO;
     StatusDAO statusDAO;
     CategoryDAO categoryDAO;
+    PriorityDAO priorityDAO;
 
     @FXML
-    private void handleButton1Action(ActionEvent event) {
+    private void statusButtonEvent(ActionEvent event) {
         HashMap<String, Integer> projectList = new HashMap<String, Integer>();
         int totalProjects = projectDAO.getAll().size();
         for (Status status : (ArrayList<Status>) statusDAO.getAll()) {
@@ -42,22 +43,42 @@ public class StatisticController implements Initializable {
     }
 
     @FXML
-    private void handleButton2Action(ActionEvent event) {
-
+    private void categoriesButtonEvent(ActionEvent event) {
+        HashMap<String, Integer> categoriesList = new HashMap<String, Integer>();
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+        int totalProjects = projectDAO.getAll().size();
+        for (Category category : (ArrayList<Category>) categoryDAO.getAll()) {
+            categoriesList.put(category.getName(), projectDAO.getProjectsCountByCategory(category.getId()));
+        }
+        for (Map.Entry<String, Integer> data : categoriesList.entrySet()) {
+            double parentage = 100.0 * data.getValue() / totalProjects;
+            pieChartData.add(new PieChart.Data(data.getKey() + " %" + String.format("%.2f", parentage), data.getValue()));
+        }
+        piechart.setTitle("Categories Statistic");
+        piechart.setData(pieChartData);
     }
 
     @FXML
-    private void handleButtonClearAction(ActionEvent event) {
+    private void priorityButtonEvent(ActionEvent event) {
+        HashMap<String, Integer> priorityList = new HashMap<String, Integer>();
         ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
-        piechart.setTitle("");
+        int totalPriority = priorityDAO.getAll().size();
+//        for (Priority priority : (ArrayList<Priority>) priorityDAO.getAll()) {
+//          //  priorityList.put(priority.getName(), priority.(priority.getId()));
+//        }
+        for (Map.Entry<String, Integer> data : priorityList.entrySet()) {
+            double parentage = 100.0 * data.getValue() / totalPriority;
+            pieChartData.add(new PieChart.Data(data.getKey() + " %" + String.format("%.2f", parentage), data.getValue()));
+        }
+        piechart.setTitle("Categories Statistic");
         piechart.setData(pieChartData);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
         projectDAO = ProjectDAO.getInstance();
         statusDAO = StatusDAO.getInstance();
         categoryDAO = CategoryDAO.getInstance();
+        priorityDAO = PriorityDAO.getInstance();
     }
 }
