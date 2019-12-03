1af48f90ffcc6297563b301fe6494d2b606c6e3b
Ghaith Darwish <ghaith.darwish01@stclairconnect.ca>
2019-12-03 11:32:47 -0500

    Read data for charts from database

--- src/controllers/StatisticController.java
+++ src/controllers/StatisticController.java
@@ -1,5 +1,9 @@
 package controllers;
 
+import Database.CSP.Status.Status;
+import Database.CSP.Status.StatusDAO;
+import Database.Project.Project;
+import Database.Project.ProjectDAO;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -8,29 +12,41 @@ import javafx.fxml.Initializable;
 import javafx.scene.chart.PieChart;
 
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class StatisticController implements Initializable {
 
     @FXML
     private PieChart piechart;
+    ProjectDAO projectDAO;
+    StatusDAO statusDAO;
 
+    HashMap<String, Integer> dataList = new HashMap<String, Integer>();
     @FXML
     private void handleButton1Action(ActionEvent event) {
-        ObservableList<PieChart.Data> pieChartData =
-                FXCollections.observableArrayList(
-                        new PieChart.Data("January", 100),
-                        new PieChart.Data("February", 200),
-                        new PieChart.Data("March", 50),
-                        new PieChart.Data("April", 75),
-                        new PieChart.Data("May", 110),
-                        new PieChart.Data("June", 300),
-                        new PieChart.Data("July", 111),
-                        new PieChart.Data("August", 30),
-                        new PieChart.Data("September", 75),
-                        new PieChart.Data("October", 55),
-                        new PieChart.Data("November", 225),
-                        new PieChart.Data("December", 99));
+
+        for (Status status : (ArrayList<Status>) statusDAO.getAll()) {
+            dataList.put(status.getName(), projectDAO.getProjectsCountByStatus(status.getId()));
+        }
+
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+//                new PieChart.Data("January", 100),
+//                new PieChart.Data("February", 200),
+//                new PieChart.Data("March", 50),
+//                new PieChart.Data("April", 75),
+//                new PieChart.Data("May", 110),
+//                new PieChart.Data("June", 300),
+//                new PieChart.Data("July", 111),
+//                new PieChart.Data("August", 30),
+//                new PieChart.Data("September", 75),
+//                new PieChart.Data("October", 55),
+//                new PieChart.Data("November", 225),
+//                new PieChart.Data("December", 99));
+
+
+        for(Map.Entry<String, Integer> data : dataList.entrySet()) {
+            pieChartData.add(new PieChart.Data(data.getKey(), data.getValue()));
+        }
 
         piechart.setTitle("Monthly Record");
         piechart.setData(pieChartData);
@@ -63,5 +79,7 @@ public class StatisticController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        projectDAO = ProjectDAO.getInstance();
+        statusDAO = StatusDAO.getInstance();
     }
 }
