cb50bb5e0282cf3834e9b9338733c882c78be497
Ndricim Strazimiri <ns10@myscc.ca>
2019-12-03 01:55:57 -0500

    Finished project Update including tasks update * Finished tasks displaying with project name * task Update can be done also on tasks table, where the open status can be updated

--- src/Forms/TaskFormController.java
+++ src/Forms/TaskFormController.java
@@ -1,5 +1,10 @@
 package Forms;
 
+import Database.Project.Project;
+import Database.Project.ProjectDAO;
+import Database.Task.Task;
+import Database.Task.TaskDAO;
+import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXToggleButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,8 +13,10 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
@@ -22,15 +29,55 @@ public class TaskFormController implements Initializable {
     @FXML
     private JFXToggleButton closeTask;
 
+    @FXML
+    JFXTextField name;
+    @FXML
+    JFXTextField project;
+    @FXML
+    VBox errorDisplay;
+
+
+
+    public static Task editingTask;
+
     public static boolean updateForm = false;
 
+    TaskDAO taskDAO=TaskDAO.getInstance();
+    ProjectDAO projectDAO=ProjectDAO.getInstance();
+    Project thisTasksProject=projectDAO.get(editingTask.getProject()).get();
 
     public void processForm(ActionEvent actionEvent) {
+        LinkedList<String> errors = new LinkedList<>();
+        if (name.getText().isEmpty() || name.getText().length() < 10) {
+            errors.add("Task name should contain at least 10 letters");
+            name.setStyle("-fx-border-color: red;");
+        } else {
+            name.setStyle("-fx-border-color: none;");
+        }
+
+        errorDisplay.getChildren().clear();
+        if (errors.size() > 0) {
+            for (int i = 0; i < errors.size(); i++) {
+                Label errorLabel = new Label(errors.get(i));
+                errorDisplay.getChildren().add(errorLabel);
+            }
+        } else {
+            errorDisplay.getChildren().add(new Label("ALL SET"));
+            editingTask.setName(name.getText());
+            byte taskOpen=1;
+            if(closeTask.isSelected()){
+                taskOpen=0;
+            }
+            editingTask.setOpen(taskOpen);
+            taskDAO.update(editingTask);
+        }
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         closeTask.setStyle("visibility: hidden;");
+        project.setText(thisTasksProject.getTitle());
+        name.setText(editingTask.getName());
         if (updateForm) {
             title.setText("Edit Task");
             submitButton.setText("Submit");
@@ -46,11 +93,13 @@ public class TaskFormController implements Initializable {
         if (closeTask.isSelected()) {
             Optional<ButtonType> result = alert.showAndWait();
             if (result.get() == ButtonType.OK) {
-                title.setText("Closed!"); // test
+                title.setText("Task: "+name.getText()+" is Closed!");
                 // database query should go here!
             } else {
                 closeTask.setSelected(false);
             }
+        }else{
+            title.setText("Task: "+name.getText());
         }
     }
 }
