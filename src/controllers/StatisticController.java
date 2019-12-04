d6904a27edfb10bf1b3fddb033faba0ff88dbdd0
Ghaith Darwish <ghaith.darwish01@stclairconnect.ca>
2019-12-03 23:41:28 -0500

    Added Date Statistic

--- src/controllers/StatisticController.java
+++ src/controllers/StatisticController.java
@@ -13,7 +13,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.PieChart;
+import javafx.scene.chart.*;
 
 import java.net.URL;
 import java.util.*;
@@ -22,17 +22,22 @@ public class StatisticController implements Initializable {
 
     @FXML
     private PieChart piechart;
+
+
+    @FXML
+    private CategoryAxis xAxis;
+    @FXML
+    private NumberAxis yAxis;
+
+    @FXML
+    private LineChart<String, Number> lineChart;
+
+
     ProjectDAO projectDAO;
-    StatusDAO statusDAO;
-    CategoryDAO categoryDAO;
-    PriorityDAO priorityDAO;
-
     CSPDAO cspdao;
-
     ObservableList<PieChart.Data> pieChartData;
     HashMap<String, Integer> statisticList;
     ArrayList<Project> openProject;
-
 
     @FXML
     private void statusButtonEvent(ActionEvent event) {
@@ -66,6 +71,35 @@ public class StatisticController implements Initializable {
         }
         piechart.setTitle(title + " Statistic");
         piechart.setData(pieChartData);
+
+        lineChart.setStyle("visibility: false");
+        piechart.setStyle("visibility: true");
+    }
+
+    public void dateButtonEvent(ActionEvent event) {
+        xAxis.setLabel("Month");
+        yAxis.setLabel("Number");
+        XYChart.Series<String, Number> series = new XYChart.Series();
+        series.setName("My portfolio");
+
+        series.getData().add(new XYChart.Data("Jan", 23));
+        series.getData().add(new XYChart.Data("Feb", 14));
+        series.getData().add(new XYChart.Data("Mar", 15));
+        series.getData().add(new XYChart.Data("Apr", 24));
+        series.getData().add(new XYChart.Data("May", 34));
+        series.getData().add(new XYChart.Data("Jun", 36));
+        series.getData().add(new XYChart.Data("Jul", 22));
+        series.getData().add(new XYChart.Data("Aug", 45));
+        series.getData().add(new XYChart.Data("Sep", 43));
+        series.getData().add(new XYChart.Data("Oct", 17));
+        series.getData().add(new XYChart.Data("Nov", 29));
+        series.getData().add(new XYChart.Data("Dec", 25));
+
+        lineChart.getData().clear();
+        lineChart.getData().addAll(series);
+        lineChart.setStyle("visibility: true");
+        piechart.setStyle("visibility: false");
+
     }
 
     @Override
